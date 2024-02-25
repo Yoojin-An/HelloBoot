6b7daf926837057ff460271ed5d6d09158ff2281
안유진 <yoojin.an0@gmail.com>
2024-02-25 15:20:51 +0900

    [feat.new] 빈 톰캣 서블릿 컨테이너에 서블릿 추가
    - 서블릿 콘텍스트에 서블릿을 등록해주면서 응답의 3요소(Status Line, Header, Body)를 구현 후 '/hello' url과 매핑해준다.

--- src/main/java/yoojinspring/helloboot/HellobootApplication.java
+++ src/main/java/yoojinspring/helloboot/HellobootApplication.java
@@ -1,15 +1,39 @@
 package yoojinspring.helloboot;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-//		new Tomcat().start();
+//		new Tomcat().start(); Tomcat을 직접 start하는 게 아니라 Servlet Container에 Tomcat 서블릿 등록하는 작업을 도와주는 TomcatServletWebServerFactory를 이용한다.
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello " + name);
+				}
+			}).addMapping("/hello");
+
+
+        });
 		webServer.start();
 	}
 
