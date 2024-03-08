9e0a3d8eacc9f9e82e7d0b96b06f348f081a9a72
안유진 <yoojin.an0@gmail.com>
2024-03-08 15:17:53 +0900

    [feat.mod] HelloController를 frontcontroller에 바인딩
    - 초기 HelloController는 main 메소드가 standalone으로 동작하도록 만들기 전 Spring의 도움을 받아 /hello 경로로 GET 요청이 들어왔을 때에 대한 응답 처리 클래스였음
    - 웹 클라이언트의 모든 HTTP 요청은 1차적으로 front controller의 main 메소드가 담당하도록 이전 커밋에서 처리했고, 본 커밋에서는 Spring의 매핑 기능이 삭제된, 단순히 name 파라미터를 받아 반환값을 만들어내는 HelloController가 /hello 경로로 들어온 요청에 대한 실질적 응답을 처리하도록 바인딩함

--- src/main/java/yoojinspring/helloboot/HellobootApplication.java
+++ src/main/java/yoojinspring/helloboot/HellobootApplication.java
@@ -22,6 +22,7 @@ public class HellobootApplication {
 //		new Tomcat().start(); Tomcat을 직접 start하는 게 아니라 Servlet Container에 Tomcat 서블릿 등록하는 작업을 도와주는 TomcatServletWebServerFactory를 이용한다.
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,12 +30,16 @@ public class HellobootApplication {
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						String ret = helloController.hello(name);
+
 						resp.setStatus(HttpStatus.OK.value());
 						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("Hello " + name);
-					} else if (req.getRequestURI().equals("/user")) {
+						resp.getWriter().println(ret);
+					}
+					else if (req.getRequestURI().equals("/user")) {
 						//
-					} else {
+					}
+					else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
 				}
