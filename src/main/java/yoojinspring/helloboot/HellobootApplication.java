d68521bef1699904c55bfc6dcb0ebd2fbadd30d0
안유진 <yoojin.an0@gmail.com>
2024-03-08 12:52:59 +0900

    [feat.mod] 서블릿 컨테이너를 프론트 컨트롤러로 전환
    - 서블릿 내에서 HTTP 요청 정보를 이용해서 각 요청을 분리 -> 추후 모듈화 진행 예정
    - 정의되지 않은 HTTP 요청이 들어오면 404(NOT_FOUND) 에러 발생하도록 설정
    - front controller가 모든 URL의 요청을 처리할 수 있도록 서블릿 바인딩(매핑)을 '/*'로 변경

--- src/main/java/yoojinspring/helloboot/HellobootApplication.java
+++ src/main/java/yoojinspring/helloboot/HellobootApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -21,18 +22,23 @@ public class HellobootApplication {
 //		new Tomcat().start(); Tomcat을 직접 start하는 게 아니라 Servlet Container에 Tomcat 서블릿 등록하는 작업을 도와주는 TomcatServletWebServerFactory를 이용한다.
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					// 인증, 보안, 다국어, 공통 기능
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello " + name);
+					} else if (req.getRequestURI().equals("/user")) {
+						//
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
-
-
+			}).addMapping("/*"); // 모든 요청에 대해서 처리함
         });
 		webServer.start();
 	}
